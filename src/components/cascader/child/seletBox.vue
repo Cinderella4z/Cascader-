f9e2e71e6f5b7955f37a5e02cfce90da93d5f659
zzq <2677310408@qq.com>
2022-12-06 16:24:35 +0800

    模糊搜索选择完 下拉框自动跳转至可视区

--- src/components/cascader/child/seletBox.vue
+++ src/components/cascader/child/seletBox.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div class="box" v-show="show">
     <div v-for="(c, k) in propData" class="item" :key="c.id" @click="itemClick(c, k)"
       :class="{ 'active': k === active }" ref="box">
@@ -7,18 +6,15 @@
       {{ c.ad_name }}
     </div>
   </div>
-
-
 </template>
 <script lang="ts" setup>
 import type { Idata_tree } from '@/types/Idata';
-import { ref, toRefs, type Ref, } from 'vue';
+import { onMounted, ref, toRefs, watch, type Ref, nextTick } from 'vue';
 /*****
  *propData:父组件传来进行遍历创建
  *index: 此变量是父组件遍历子组件中的index, 为了得知此子组件是第几个
  *show:控制子组件显影
  */
-
 const props = defineProps(['index', 'propData', 'show',])
 const { propData, index, show } = toRefs(props)
 // 控制点击高亮
@@ -38,15 +34,26 @@ const itemClick = (c: Idata_tree, k: number) => {
 const setActive = () => {
   active.value = -1
 }
+const matchItem = ref()
 const setMatchItem = (name: string, index: string) => {
 
   let key = propData?.value.findIndex((item: Idata_tree) => item.ad_name === name)
   active.value = key
+  matchItem.value = box.value[key]
+
   if (typeof index !== undefined) {
     ifMatch.value = key
   }
 }
 defineExpose([setActive, setMatchItem])
+
+
+// 当模糊搜索 选择完之后 打开下拉选择框 跳到对应选择项的可视区
+watch(show as Ref, (n) => {
+  nextTick(() => {
+    matchItem.value && matchItem.value.scrollIntoView()
+  })
+})
 
 </script>
 <style lang="less" scoped>
